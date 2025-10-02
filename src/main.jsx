fe0ab7330c83ccba47a7cc991cd56aa487753a5d
unknown <biplob.code.bd@gmail.com>
2025-10-02 11:12:09 +0600

    fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,58 +1,42 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-import { createBrowserRouter, RouterProvider } from 'react-router'
-import Contact from './pages/Contact.jsx'
-import Cart from './pages/Cart.jsx'
-import About from './pages/About.jsx'
-import RootLayout from './Layouts/RootLayout.jsx'
-import Home from './pages/Home.jsx'
-import LoadingSpinner from './Components/LoadingSpinner.jsx'
-import PlantDetails from './pages/PlantDetails.jsx'
-import axios from 'axios'
-import { ToastContainer } from 'react-toastify'
-const router = createBrowserRouter([
+import { createHashRouter, RouterProvider } from "react-router-dom"; // <-- HashRouter
+import Contact from "./pages/Contact.jsx";
+import Cart from "./pages/Cart.jsx";
+import About from "./pages/About.jsx";
+import RootLayout from "./Layouts/RootLayout.jsx";
+import Home from "./pages/Home.jsx";
+import LoadingSpinner from "./Components/LoadingSpinner.jsx";
+import PlantDetails from "./pages/PlantDetails.jsx";
+import axios from "axios";
+import { ToastContainer } from "react-toastify";
+
+const router = createHashRouter([
   {
-    
-    path: '/',
+    path: "/",
     Component: RootLayout,
     hydrateFallbackElement: <LoadingSpinner />,
     children: [
+      { index: true, element: <Home /> },
+      { path: "/home", element: <Home /> },
+      { path: "/contact", element: <Contact /> },
+      { path: "/about", Component: About },
       {
-        index: true,
-        element: <Home />,
-       
-      },
-      {
-        path: '/home',
-        element: <Home />,
-      },
-      {
-        path: '/contact',
-        element: <Contact />,
-      },
-      {
-        path: '/about',
-        Component: About,
-      },
-      {
-        path: '/plant-details/:id',
+        path: "/plant-details/:id",
         Component: PlantDetails,
         loader: ({ params }) =>
           axios(`https://openapi.programming-hero.com/api/plant/${params.id}`),
       },
-      {
-        path: '/cart',
-        Component: Cart,
-      },
+      { path: "/cart", Component: Cart },
     ],
   },
-])
-createRoot(document.getElementById('root')).render(
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
-    <ToastContainer/>
-
-  </StrictMode>,
-)
+    <ToastContainer />
+  </StrictMode>
+);
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,17 +1,4 @@
-// import { use } from 'react'
-// import { CartContext } from '../Providers/CartContext'
 
-// const Cart = () => {
-//   const { cart, setCart } = use(CartContext)
-//   return (
-//     <div>
-//       {cart.map(plant => (
-//         <p key={plant.id}>{plant.name}</p>
-//       ))}
-//     </div>
-//   )
-// }
-// export default Cart
 
 
 import { use } from "react";
